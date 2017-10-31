b3ecdd4b9d1e93986755f378ac24a7ee040d4bed
franny1615 <31251485+franny1615@users.noreply.github.com>
2017-10-31 09:15:48 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-def get_state_options:
+def get_state_options():
     option = request.args['option']
     option = Markup("<option value="memes">"+ "memes" + "</option>")
 
